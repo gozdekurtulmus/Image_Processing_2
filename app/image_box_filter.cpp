3408359e557a8752adf29b8eb573e674f875d26f
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-12-16 03:03:24 +0300

    Add files via upload

---
+++ app/image_box_filter.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "ceng391/image.hpp"
+
+using namespace std;
+using ceng391::Image;
+
+int main(int argc, char** argv)
+{
+        if (argc < 2) {
+                cerr << "Usage: " << argv[0] << " <filename> [<filter-size>]" << endl;
+                return EXIT_FAILURE;
+        }
+
+        Image *img =  Image::from_png(argv[1], false);
+
+        int filter_size = 5;
+        if (argc > 2)
+                filter_size = atoi(argv[2]);
+        //img->box_filter(filter_size);
+        //img->xsave_png("/tmp/box.png");
+
+
+        img->smooth(1.5, 2.0, 3);
+        img->xsave_png("/tmp/gaussian.png");
+
+        delete img;
+        return EXIT_SUCCESS;
+}
+
+/// Local Variables:
+/// mode: c++
+/// compile-command: "make -C ../build image-box-filter"
+/// End:
